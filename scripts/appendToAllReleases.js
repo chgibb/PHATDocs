c5ecc92ac421b8f16f77281a7255671c776d6653
Chris Gibb <chris.gibb@outlook.com>
2017-07-22 10:35:04 -0400

    Refactored to put newest release at the top

--- scripts/appendToAllReleases.js
+++ scripts/appendToAllReleases.js
@@ -2,5 +2,19 @@ const fs = require("fs");
 
 module.exports = function appendToAllReleases(tag_name,branch)
 {
-    fs.appendFileSync(`docs/allReleases.md`,`${new Date}: [PHAT@${tag_name}](https://chgibb.github.io/PHATDocs/releases/${tag_name}/index) cut from ${branch}  ${"\n"}`);
+    let allReleases = JSON.parse(fs.readFileSync(`docs/allReleases.json`).toString());
+    allReleases.unshift(
+        `${new Date}: [PHAT@${tag_name}](https://chgibb.github.io/PHATDocs/releases/${tag_name}/index) cut from ${branch}`
+    );
+    fs.writeFileSync(`docs/allReleases.json`,JSON.stringify(allReleases,undefined,4));
+
+    let md = `---${"\n"}title: All Releases${"\n"}---${"\n"}`;
+    for(let i = 0; i != allReleases.length; ++i)
+    {
+            md += `${allReleases[i]}  ${"\n"}`;
+    }
+    fs.writeFileSync(
+        `docs/allReleases.md`,
+        md
+    );
 }
