0dfeacb11585df72a7cbeb1940d6290b27718e0f
Chris Gibb <chris.gibb@outlook.com>
2017-08-03 15:54:31 -0400

    Stop clobbering images when generating docs

--- scripts/generateReleaseDocsFromLatest.js
+++ scripts/generateReleaseDocsFromLatest.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+const fse = require("fs-extra");
 const walk = require("walk");
 
 const isMarkDown = /\.md/;
@@ -13,9 +14,14 @@ module.exports = async function generateReleaseDocsFromLatest(tag_name)
         //Patch all doc links to point from latest to the version of this deployment
         //Also patch occurences of $TAGNAME$ with this version
         walker.on("file",function(root,fileStats,next){
-            if(!isMarkDown.test(fileStats.name))
-                next();
             
+            if(!isMarkDown.test(fileStats.name))
+            {
+                fse.copySync(`${root}/${fileStats.name}`,`docs/docs/releases/${tag_name}/${fileStats.name}`);
+                next();
+                return;
+            }
+
             let file = fs.readFileSync(`${root}/${fileStats.name}`).toString();
             file = file.replace(/\/latest\//g,`/releases/${tag_name}/`);
             file = file.replace(/\$TAGNAME\$/g,`${tag_name}`);     
